9de8f25a1e94050f92e81734d3cd2e72a61f7bcd
BlackLanzer <ale.sensei@yahoo.it>
2015-11-05 10:18:22 +0100

    First solution

--- app.js
+++ app.js
@@ -13,9 +13,46 @@ var tmpl = ' <li id="ID">' +
            '  <h3>SENTENCE</h3>' +
            ' </li> ';
 
+var currentExerciseId = 0;
+var rightAnswers = 0;
+
 $(document).ready(function(){
 
-  
+
+	// reset the list and the result 
+	$(".sentences").html("");
+	$(".final").hide();
+
+	// populate the list
+	for (var i = 0; i < data.length; i++) {
+		$(".sentences").append(tmpl.replace("ID", i).replace("SENTENCE", data[i].phrase_en))			
+	}
+  	
+  	// show the first sentence
+  	$("#0").addClass('current');
+
+  	// button handler
+  	$(".opt-continue").click(function() {
+  		
+  		// check the answer
+  		if($(".form-control").val() == data[currentExerciseId].phrase_de)
+  		{
+  			rightAnswers++;
+  		}
+
+  		// show next exercise
+  		if (currentExerciseId < data.length-1) {
+  			$("#"+currentExerciseId).removeClass('current');
+  			currentExerciseId++;
+  			$("#"+currentExerciseId).addClass('current');
+  			$(".form-control").val("");
+  		} 
+  		else{	// or show the result
+  			$("#tot-good").html(rightAnswers);
+  			$("#tot").html(data.length);
+  			$(".final").show();
+  		};
+  	});
 });
 
 
